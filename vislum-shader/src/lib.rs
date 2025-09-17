2fbd264825bd98dbb69398554051db932a752f95
Victor Lima <victofrancovl@gmail.com>
2025-09-17 00:14:08 -0300

    feat: expose more types in vislum-shader prelude

--- vislum-shader/src/lib.rs
+++ vislum-shader/src/lib.rs
@@ -2,6 +2,6 @@ pub mod composer;
 pub mod directive;
 
 pub mod prelude {
-    pub use crate::composer::ShaderComposer;
+    pub use crate::composer::{ComposeError, ComposeErrorType, ShaderComposer};
     pub use crate::directive::collect_includes;
 }
